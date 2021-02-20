f96611b0ce65c17a8602ab7ebcddffeb53a694ef
Sergey Krivov <skrivov@tsconsulting.com>
2021-02-20 11:36:14 +0300

    Завершен алгоритм добавления вещей. Подключил PostgreSQL. Добавлена регистрация пользователя.

--- main.py
+++ main.py
@@ -15,11 +15,16 @@ bot.
 """
 
 import logging
-import uuid
+import psycopg2
+from psycopg2 import Error, sql
+from psycopg2._psycopg import cursor
+from psycopg2.sql import SQL
 
-from things import add_thing
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, ConversationHandler
+import things
+from things import add_thing, add_photo, add_cost, add_name, add_discount, accept_thing
+from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, ConversationHandler, \
+    CallbackQueryHandler
 
 # Enable logging
 logging.basicConfig(
@@ -33,7 +38,32 @@ logger = logging.getLogger(__name__)
 # context. Error handlers also receive the raised TelegramError object in error.
 def start(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
+    user = update.message.from_user
+    reg = [[InlineKeyboardButton("Регистрация", callback_data='reg')]]
+    update.message.reply_text("Привет, я бот и я помогу тебе определиться в покупкой вещей. Просто следуй моим "
+                              "инструкциям.\n"
+                              "Для начала нажми на кнопку *Регистрация.", reply_markup=InlineKeyboardMarkup(reg))
+
+
+def welcome(update: Update, context: CallbackContext) -> None:
+    user = update.callback_query.from_user
+    update.callback_query.from_user.send_message("Приятно познакомиться %s" % user.first_name)
+    try:
+        # Подключение к существующей базе данных
+        connection = psycopg2.connect(user="postgres",
+                                      # пароль, который указали при установке PostgreSQL
+                                      password="15021994",
+                                      host="127.0.0.1",
+                                      port="5432",
+                                      database="postgres")
+        insert_query = SQL("INSERT INTO users (username, first_name, last_name) VALUES (%s, %s, %s);")
+        print(sql.SQL(insert_query), (user.username, user.first_name, user.last_name,))
+        cursor.execute(insert_query, (user.username, user.first_name, user.last_name,))
+        connection.commit()
+        print("1 запись успешно вставлена")
+
+    except (Exception, Error) as error:
+        print("Ошибка при работе с PostgreSQL", error)
 
 
 def help_command(update: Update, context: CallbackContext) -> None:
@@ -41,31 +71,10 @@ def help_command(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Help!')
 
 
-def echo(update: Update, context: CallbackContext) -> None:
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
-
-
-def get_photo(update: Update, context: CallbackContext) -> None:
-    # update.message.reply_text("Классное фото!")
-    photos = update.message.photo
-    file_name = str(uuid.uuid4())
-    context.bot.get_file(photos[-1]).download("./photos/" + file_name + ".jpg")
-
-
-def add_thing1(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text("Как назовём эту вещь?")
-    return PHOTO
-
-
-def add_photo1(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text("Hi")
-
-
 def main():
     """Start the bot."""
     # Create the Updater and pass it your bot's token.
-    updater = Updater("1382236154:AAGeKzECdiAKTeleXqnnB9e2bBzV5itBWH0")
+    updater = Updater("1382236154:AAGYgwaHWxI_F03b6ptqcx1fbNYjLMpFrnk", use_context=True)
 
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
@@ -73,16 +82,18 @@ def main():
     # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
-    dispatcher.add_handler(CommandHandler("добавить", help_command))
-
+    dispatcher.add_handler(CallbackQueryHandler(welcome))
     # on noncommand i.e message - echo the message on Telegram
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
-    dispatcher.add_handler(MessageHandler(Filters.photo, get_photo))
-    conv_handler = ConversationHandler(entry_points=[ConversationHandler("добавить", add_thing1)],
-                                       states={
-                                           PHOTO: [MessageHandler(Filters.photo, add_photo)]
-                                       },
-                                       fallbacks=[])
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("add", add_thing)],
+        states={
+            things.NAME: [MessageHandler(Filters.text, add_name)],
+            things.PHOTO: [MessageHandler(Filters.photo | Filters.text, add_photo)],
+            things.COST: [MessageHandler(Filters.text, add_cost)],
+            things.DISCOUNT: [MessageHandler(Filters.text, add_discount)]
+        },
+        fallbacks=[],
+    )
     dispatcher.add_handler(conv_handler)
 
     # Start the Bot
--- things.py
+++ things.py
@@ -1,19 +1,114 @@
 from telegram import Update
-from telegram.ext import CallbackContext
-
-
-
-
-def add_thing(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text("Как назовём эту вещь?")
-    return PHOTO
-
-def add_photo(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text("Hi")
-
+from telegram.ext import CallbackContext, ConversationHandler
+import uuid
+import psycopg2
+from psycopg2 import Error
 
 
 class Thing:
 
-    def __init__(self):
+    def __init__(self, name, photo, cost, discount):
+        self.name = name
+        self.photo = photo
+        self.cost = cost
+        self.discount = discount
         pass
+
+
+PHOTO, NAME, COST, DISCOUNT, ACCEPT = range(5)
+t1 = Thing(0, 0, 0, 0)
+
+
+def add_thing(update: Update, context: CallbackContext) -> int:
+    update.message.reply_text("Как назовём эту вещь?")
+    return NAME
+
+
+def add_name(update: Update, context: CallbackContext) -> int:
+    name = update.message.text
+    t1.name = name
+    update.message.reply_text("Окей, давай добавим фотографии " + name)
+    return PHOTO
+
+
+def add_photo(update: Update, context: CallbackContext) -> int:
+    if update.message.text:
+        if update.message.text.lower().strip() == "стоп":
+            update.message.reply_text("Теперь отправь стоимость вещи")
+            return COST
+        else:
+            update.message.reply_text("Не понял тебя, напиши ещё раз.")
+    elif update.message.photo:
+        try:
+            photos = update.message.photo
+            file_name = str(uuid.uuid4())
+            t1.photo = photos[-1].file_id  # добавить массив фоток
+            context.bot.get_file(photos[-1]).download("./photos/" + file_name + ".jpg")
+        except Exception:
+            print(Exception)
+            update.message.reply_text("Опс, что-то пошло не так!")
+            return ConversationHandler.END
+        finally:
+            update.message.reply_text("Фото успешно добавлено.\n"
+                                      "Если фото больше нет, отправьте \"Стоп\"")
+            return PHOTO
+    else:
+        update.message.reply_text("Отправь текст или фото")
+
+
+def add_cost(update: Update, context: CallbackContext) -> int:
+    cost = update.message.text
+    t1.cost = cost
+    update.message.reply_text("Отправьте размер (%) скидки ")
+    return DISCOUNT
+
+
+def add_discount(update: Update, context: CallbackContext) -> int:
+    discount = update.message.text
+    t1.discount = discount
+    accept_thing(update, context)
+    return ConversationHandler.END
+
+
+def accept_thing(update: Update, context: CallbackContext) -> int:
+    try:
+        # Подключение к существующей базе данных
+        connection = psycopg2.connect(user="postgres",
+                                      # пароль, который указали при установке PostgreSQL
+                                      password="15021994",
+                                      host="127.0.0.1",
+                                      port="5432",
+                                      database="discount")
+
+        # Курсор для выполнения операций с базой данных
+        cursor = connection.cursor()
+        # Распечатать сведения о PostgreSQL
+        print("Информация о сервере PostgreSQL")
+        print(connection.get_dsn_parameters(), "\n")
+        # Выполнение SQL-запроса
+        cursor.execute("SELECT version();")
+        # Получить результат
+        record = cursor.fetchone()
+        print("Вы подключены к - ", record, "\n")
+
+    except (Exception, Error) as error:
+        print("Ошибка при работе с PostgreSQL", error)
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+            print("Соединение с PostgreSQL закрыто")
+
+    total_price = int(t1.cost) * (100-int(t1.discount)) * 0.01
+    print(t1.name)
+    print(t1.photo)
+    print(t1.cost)
+    print(t1.discount)
+    print(total_price)
+    update.message.reply_text("Итак, вот что у нас получилось: "
+                              "\nНазвание - " + t1.name +
+                              "\nСтоимость - " + t1.cost +
+                              "\nСкидка - " + t1.discount + "%"
+                              "\nСтоимость с учётом скидки - " + str(int(total_price)) +
+                              "\nФотографии: ")
+    context.bot.send_photo(update.message.chat_id, t1.photo)
